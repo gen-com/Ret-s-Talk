5505992cec4f021875b9b231edf0bd6c086222f3
Hanseung Cho <1win2@naver.com>
2024-11-04 17:05:10 +0900

    chore: Lint 적용

---
+++ RetsTalk/RetsTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RetsTalk
+//
+//  Created by HanSeung on 11/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

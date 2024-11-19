c18d4ec12f0cf29e55c0d306590ac5e689b95623
구병조(GEN) <31850425+gen-com@users.noreply.github.com>
2024-11-19 10:53:52 +0900

    저장소 인터페이스 리팩터링#49 (#50)
    * refactor: 레거시 파일 삭제
    
    * refactor: 저장소 추상 리팩터링
    
    * refactor: 엔티티 추상 수정
    
    * refactor: 오타 및 네이밍 수정

---
+++ RetsTalk/RetsTalk/Persistent/Persistable.swift
@@ -0,0 +1,33 @@
+//
+//  Persistable.swift
+//  RetsTalk
+//
+//  Created by MoonGoon on 11/11/24.
+//
+
+import Foundation
+
+protocol Persistable {
+    /// 로컬 저장소에 엔티티 데이터를 추가합니다.
+    /// - Parameter entities: 추가할 엔티티 배열.
+    /// - Returns: 추가된 데이터.
+    func add<Entity>(contentsOf entities: [Entity]) async throws -> [Entity] where Entity: EntityRepresentable
+    /// 로컬 저장소에서 요청 조건에 맞는 엔티티 데이터를 불러옵니다.
+    /// - Parameter request: 요청 조건.
+    /// - Returns: 엔티티 데이터.
+    func fetch<Entity>(
+        by request: any PersistFetchRequestable<Entity>
+    ) async throws -> [Entity] where Entity: EntityRepresentable
+    /// 로컬 저장소의 엔티티 데이터에 대한 최신화를 수행합니다.
+    /// - Parameters:
+    ///   - entity: 최신화할 엔티티 값.
+    ///   - predicate: 최신화 할 엔티티 데이터를 찾기 위한 조건문.
+    /// - Returns: 최신화 된 엔티티 데이터.
+    func update<Entity>(
+        to entity: Entity,
+        forMatching predicate: NSPredicate
+    ) async throws -> Entity where Entity: EntityRepresentable
+    /// 로컬 저장소에서 엔티티 데이터를 제거합니다.
+    /// - Parameter entities: 제거할 엔티티 데이터.
+    func delete<Entity>(contentsOf entities: [Entity]) async throws where Entity: EntityRepresentable
+}

0f123f419c55d0e186870be62f16eb69d17092af
Hanseung Cho <110288835+1win2@users.noreply.github.com>
2024-12-01 15:31:04 +0900

    UserSettingManager: 사용자 ID 반환/부여하는 기능 (#154)
    * feat: 사용자설정 매니저 인터페이스 수정
    
    * feat: 초기화 함수 구현 및 랜덤 닉네임 기능
    
    * feat: 사용자정보의 userID 데이터 추가
    
    * UserSettingManager.swift 업데이트
    
    Co-authored-by: 구병조(GEN) <31850425+gen-com@users.noreply.github.com>
    
    * style: 리뷰 반영
    
    ---------
    
    Co-authored-by: 구병조(GEN) <31850425+gen-com@users.noreply.github.com>

--- RetsTalk/RetsTalk/User/Model/UserSettingManager.swift
+++ RetsTalk/RetsTalk/User/Model/UserSettingManager.swift
@@ -6,6 +6,7 @@
 //
 
 import Combine
+import Foundation
 
 final class UserSettingManager: UserSettingManageable, @unchecked Sendable, ObservableObject {
     @Published var userData: UserData = .init(dictionary: [:])
@@ -19,17 +20,30 @@ final class UserSettingManager: UserSettingManageable, @unchecked Sendable, Obse
     
     // MARK: UserSettingManageable conformance
     
+    func initialize() async -> UUID? {
+        do {
+            let request = PersistFetchRequest<UserData>(fetchLimit: 1)
+            let fetchedData = try await userDataStorage.fetch(by: request)
+            guard let storedUserData = fetchedData.first
+            else { return initializeUserData() }
+
+            await MainActor.run {
+                userData = storedUserData
+            }
+            return UUID(uuidString: userData.userID)
+        } catch {
+            return nil
+        }
+    }
+    
     func fetch() {
         let request = PersistFetchRequest<UserData>(fetchLimit: 1)
         Task {
             let fetchedData = try await userDataStorage.fetch(by: request)
-            guard fetchedData.isNotEmpty, let fetchedData = fetchedData.first else {
-                initiateUserData()
-                return
-            }
+            guard let storedUserData = fetchedData.first else { return }
             
             await MainActor.run {
-                userData = fetchedData
+                userData = storedUserData
             }
         }
     }
@@ -42,7 +56,14 @@ final class UserSettingManager: UserSettingManageable, @unchecked Sendable, Obse
             }
         }
     }
+    
+    // MARK: UserData Handling Method
 
+    private func initializeUserData() -> UUID {
+        let newUserID = UUID()
+        let newNickname = randomNickname()
+        let newUserData = UserData(dictionary: ["userID": newUserID.uuidString, "nickname": newNickname])
+      
     func updateCloudSyncState(state isOn: Bool) {
         userData.isCloudSyncOn = isOn
         update(to: userData)
@@ -55,12 +76,31 @@ final class UserSettingManager: UserSettingManageable, @unchecked Sendable, Obse
 
     private func initiateUserData() {
         Task {
-            let addedData = try await userDataStorage.add(contentsOf: [UserData(dictionary: [:])])
+            let addedData = try await userDataStorage.add(contentsOf: [newUserData])
             guard let addedData = addedData.first else { return }
             
             await MainActor.run {
                 userData = addedData
             }
         }
+        return newUserID
+    }
+    
+    private func randomNickname() -> String {
+        let adjective = Texts.nicknameComposingAdjectives.randomElement() ?? ""
+        let noun = Texts.nicknameComposingNoun
+        return adjective + " " + noun
+    }
+}
+
+// MARK: - Constants
+
+private extension UserSettingManager {
+    enum Texts {
+        static let nicknameComposingAdjectives = [
+            "게임하는", "산책하는", "야근하는", "공상하는",
+            "폭식하는", "달리는", "춤추는", "헤엄치는", "노래하는",
+        ]
+        static let nicknameComposingNoun = "부덕이"
     }
 }

5f1c4f5344e683082298bf0afd8b7de78df592ad
김민석|Minseok Kim <alstjr7437@naver.com>
2024-12-02 16:22:54 +0900

    feat: Persistable이 Sendable 하게 변경

--- RetsTalk/RetsTalk/Persistent/Implementation/UserDefaultsManager.swift
+++ RetsTalk/RetsTalk/Persistent/Implementation/UserDefaultsManager.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-actor UserDefaultsManager: Persistable {
+final class UserDefaultsManager: Persistable {
     private let userDefaultsContainer: UserDefaults
 
     init(container: UserDefaults = .standard) {

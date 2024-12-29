bd989844145fa73482a7c70ae27350af51322c2b
Koo <31850425+gen-com@users.noreply.github.com>
2024-12-30 02:44:16 +0900

    feat: 플레이스홀더 텍스트뷰 추가

---
+++ RetsTalk/RetsTalk/RetrospectChat/View/PlaceholderTextView.swift
@@ -0,0 +1,150 @@
+//
+//  PlaceholderTextView.swift
+//  RetsTalk
+//
+//  Created on 12/14/24.
+//
+
+import UIKit
+
+// MARK: - Delegation
+
+@MainActor
+protocol PlaceholderTextViewDelegate: AnyObject {
+    func textViewDidChange(_ placeholderTextView: PlaceholderTextView)
+}
+
+extension PlaceholderTextViewDelegate {
+    func textViewDidChange(_ placeholderTextView: PlaceholderTextView) {}
+}
+
+// MARK: - View
+
+final class PlaceholderTextView: BaseView {
+    
+    // MARK: Property
+    
+    private(set) var fittingContentSize: CGSize = .zero
+    
+    var text: String {
+        textView.text
+    }
+    
+    var placeholderText: String? {
+        didSet { placeholderLabel.text = placeholderText }
+    }
+    var isScrollEnabled: Bool {
+        get { textView.isScrollEnabled }
+        set { textView.isScrollEnabled = newValue }
+    }
+    
+    // MARK: Delegate
+    
+    weak var delegate: PlaceholderTextViewDelegate?
+    
+    // MARK: Subviews
+    
+    private let textView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .clear
+        textView.font = .appFont(.body)
+        return textView
+    }()
+    private let placeholderLabel: UILabel = {
+        let label = UILabel()
+        label.font = .appFont(.body)
+        label.textColor = .placeholderText
+        return label
+    }()
+    
+    // MARK: Custom lifecycle
+    
+    override func setupSubviews() {
+        super.setupSubviews()
+        
+        addSubview(textView)
+        textView.addSubview(placeholderLabel)
+    }
+    
+    override func setupLayouts() {
+        super.setupLayouts()
+        
+        setupTextViewLayouts()
+        setupPlaceholderLabelLayouts()
+    }
+    
+    override func setupDelegation() {
+        super.setupDelegation()
+        
+        textView.delegate = self
+    }
+    
+    // MARK: Text edit
+    
+    func clearText() {
+        textView.text.removeAll()
+        textViewDidChange(textView)
+    }
+}
+
+// MARK: - UITextViewDelegate conformance
+
+extension PlaceholderTextView: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        placeholderLabel.isHidden = textView.text.isNotEmpty
+        fittingContentSize = textView.sizeThatFits(CGSize(width: CGFloat.infinity, height: .infinity))
+        delegate?.textViewDidChange(self)
+    }
+}
+
+// MARK: - Subview layouts
+
+fileprivate extension PlaceholderTextView {
+    private func setupTextViewLayouts() {
+        textView.textContainerInset = UIEdgeInsets(
+            top: Metrics.textPadding,
+            left: .zero,
+            bottom: Metrics.textPadding,
+            right: .zero
+        )
+        textView.textContainer.lineFragmentPadding = Metrics.textPadding
+        
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            textView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            textView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            textView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+    
+    private func setupPlaceholderLabelLayouts() {
+        placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            placeholderLabel.topAnchor.constraint(
+                equalTo: textView.topAnchor,
+                constant: Metrics.textPadding
+            ),
+            placeholderLabel.leadingAnchor.constraint(
+                equalTo: textView.leadingAnchor,
+                constant: Metrics.textPadding
+            ),
+            placeholderLabel.trailingAnchor.constraint(
+                equalTo: textView.trailingAnchor,
+                constant: -Metrics.textPadding
+            ),
+            placeholderLabel.bottomAnchor.constraint(
+                equalTo: textView.bottomAnchor,
+                constant: -Metrics.textPadding
+            ),
+        ])
+    }
+}
+
+// MARK: - Costants
+
+fileprivate extension PlaceholderTextView {
+    enum Metrics {
+        static let textPadding = 5.0
+    }
+}
